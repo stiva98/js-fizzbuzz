f328e4a9e6da8c3d117ba35abb67bdab21b6697a
stiva98 <samustiva9810@gmail.com>
2023-06-14 18:07:11 +0200

    terminato bonus 2

--- js/scripts.js
+++ js/scripts.js
@@ -5,25 +5,29 @@ for (let numBer = 1; numBer <= 100; numBer++) {
     if (numBer %3 ==0 && numBer %5 ==0){
         //console.log('FizzBuzz')
         const li=document.createElement('li');
-        li.innerHTML='FizzBuzz'
+        li.innerHTML='FizzBuzz';
+        li.classList.add('fizzbuzz');
         ul.append(li);
     }
     else if (numBer %5 ==0){
         //console.log('Buzz')
         const li=document.createElement('li');
-        li.innerHTML='Buzz'
+        li.innerHTML='Buzz';
+        li.classList.add('buzz');
         ul.append(li);
     }
     else if (numBer %3 ==0){
         //console.log('Fizz')
         const li=document.createElement('li');
-        li.innerHTML='Fizz'
+        li.innerHTML='Fizz';
+        li.classList.add('fizz');
         ul.append(li);
     }
     else {
         //console.log(numBer)
         const li=document.createElement('li');
-        li.innerHTML=numBer
+        li.innerHTML=numBer;
+        li.classList.add('numbero');
         ul.append(li);
     }
 }
